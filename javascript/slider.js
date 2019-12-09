0fc600622ac6f99ec55fedea63c604ff6944cee8
zhus-dika <zhus.dinara@gmail.com>
2019-12-10 02:19:22 +0600

    added slider behavior to reviews section

---
+++ javascript/slider.js
@@ -0,0 +1,21 @@
+const 
+ sliderItem = document.querySelectorAll(".user__photo"),
+ reviewsList= document.querySelector(".reviews__list"),
+ sliderListLength = sliderItem.length;
+ const step = 1100;
+ var item = 0;
+ sliderItem[item].style.border = "6px solid #0f5a47";
+ for (let i = 0; i < sliderListLength; i++) {
+    sliderItem[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        var currentRight = 0;
+        currentRight += i * step;
+        reviewsList.style.right = currentRight + "px";
+        sliderItem[i].style.border = "6px solid #0f5a47";
+        if (item != i)
+            sliderItem[item].style.border = "6px solid transparent";
+        item = i;
+    });
+ }
+ 
+
