754e837d7d757b34726e7d16332bb1e938b63ac1
zhus-dika <zhus.dinara@gmail.com>
2019-12-16 15:30:22 +0600

    corrected team section

--- javascript/team.js
+++ javascript/team.js
@@ -1,5 +1,6 @@
 const memberDescription = document.querySelectorAll(".member__description"),
-memberName = document.querySelectorAll(".member__name-link");
+memberName = document.querySelectorAll(".member__name-link"),
+afterIcon = document.querySelectorAll(".member__name-after__icon");
 membersNum = memberDescription.length;
 var flag = [];
 for (let i = 0; i < membersNum; i++) {
@@ -8,10 +9,12 @@ for (let i = 0; i < membersNum; i++) {
         e.preventDefault();
         e.stopPropagation();
         if (flag[i]) {
-            memberDescription[i].style.height= '0';            
+            memberDescription[i].style.height= '0'; 
+            afterIcon[i].style.transform = 'rotate(0deg)';           
             flag[i] = 0;
         } else {
             memberDescription[i].style.height= 'auto';
+            afterIcon[i].style.transform = 'rotate(180deg)';
             flag[i] = 1;
         }
     });
