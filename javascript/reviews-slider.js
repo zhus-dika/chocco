c352c56887b2b821333bedda28affe2ebfe8ff21
zhus-dika <zhus.dinara@gmail.com>
2019-12-16 12:38:56 +0600

    3d ed: corrected reviews & order sections

--- javascript/reviews-slider.js
+++ javascript/reviews-slider.js
@@ -1,16 +1,16 @@
 const 
  sliderItem = document.querySelectorAll(".user__photo"),
  reviewsList= document.querySelector(".reviews__list"),
- review= document.querySelector(".user__review"),
+ review= document.querySelectorAll(".user__review"),
  sliderListLength = sliderItem.length;
- var step = review.offsetWidth;
+ var stepRev = review[0].offsetWidth;
  var item = 0;
  sliderItem[item].style.border = "6px solid #0f5a47";
  for (let i = 0; i < sliderListLength; i++) {
     sliderItem[i].addEventListener("click", function(e) {
         e.preventDefault();
         var currentRight = 0;
-        currentRight += i * step;
+        currentRight += i * stepRev;
         reviewsList.style.right = currentRight + "px";
         sliderItem[i].style.border = "6px solid #0f5a47";
         if (item != i)
