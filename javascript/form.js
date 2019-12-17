41599672725c3f9f1a241cabd0d90ee897645e6b
zhus-dika <zhus.dinara@gmail.com>
2019-12-17 20:00:23 +0600

    added overlay to order form

--- javascript/form.js
+++ javascript/form.js
@@ -12,7 +12,7 @@ function validateForm(orderForm) {
     }
 }
 const sendButton = document.querySelector('#order__form-button'),
-form = document.querySelector(".order__form-tag");
+form = document.querySelector("#order-form");
 
 sendButton.addEventListener('click', e => {
     e.preventDefault();
@@ -26,6 +26,40 @@ sendButton.addEventListener('click', e => {
     request.open("POST", "https://webdev-api.loftschool.com/sendmail");
     request.send(formData);
     request.addEventListener('load', () => {
+        const successOverlay = createOverlay(request.response.message);
         console.log(request.response);
+        document.body.appendChild(successOverlay);
     });
 });
+
+function createOverlay(content) {
+  const overlayElement = document.createElement("div");
+  overlayElement.classList.add("overlay");
+  overlayElement.addEventListener("click", function(e) {
+    if (e.target === overlayElement) {
+      closeElement.click();
+    }
+  });
+
+  const containerElement = document.createElement("div");
+  containerElement.classList.add("container__overlay");
+
+  const contentElement = document.createElement("div");
+  contentElement.classList.add("content");
+  contentElement.innerHTML = content;
+
+  const closeElement = document.createElement("a");
+  closeElement.classList.add("close");
+  closeElement.textContent = "x";
+  closeElement.href = "#";
+  closeElement.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.body.removeChild(overlayElement);
+  });
+
+  overlayElement.appendChild(containerElement);
+  containerElement.appendChild(closeElement);
+  containerElement.appendChild(contentElement);
+
+  return overlayElement;
+}
