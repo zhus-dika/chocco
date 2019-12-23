a709190aa642e8189dbcba2079944f135205cda3
zhus-dika <zhus.dinara@gmail.com>
2019-12-23 18:22:20 +0600

    3d ed: added active class to reviews slider & renamed some classes

--- javascript/index.js
+++ javascript/index.js
@@ -68,21 +68,22 @@ function createOverlay(content) {
 const 
  sliderItem = document.querySelectorAll(".user__photo"),
  reviewsList= document.querySelector(".reviews__list"),
+ item = document.querySelectorAll(".reviews__slider-item"),
  review= document.querySelectorAll(".user__review"),
  sliderListLength = sliderItem.length;
+ var itemNum = 0;
  var stepRev = review[0].offsetWidth;
- var item = 0;
- sliderItem[item].style.border = "6px solid #0f5a47";
  for (let i = 0; i < sliderListLength; i++) {
     sliderItem[i].addEventListener("click", function(e) {
         e.preventDefault();
         var currentRight = 0;
         currentRight += i * stepRev;
         reviewsList.style.right = currentRight + "px";
-        sliderItem[i].style.border = "6px solid #0f5a47";
-        if (item != i)
-            sliderItem[item].style.border = "6px solid transparent";
-        item = i;
+        if (itemNum != i) {
+          item[i].classList.add("slider__active");
+          item[itemNum].classList.remove("slider__active");
+          itemNum = i;
+        }
     });
  }
  
